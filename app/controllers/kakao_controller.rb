632115a9187865c8af7f2e5eda9f7b76b19e7a6a
DESKTOP-7DVLGQ9\my <dlehgo14@likelion.org>
2018-07-12 20:53:23 +0900

    수정

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -16,7 +16,7 @@ class KakaoController < ApplicationController
 				message: {
 					text: "엉터리생고기 교대역점입니다.",
 					photo: {
-						url: "/다람쥐.png",
+						url: "https://afternoon-reef-22396.herokuapp.com/%EB%8B%A4%EB%9E%8C%EC%A5%90.png",
 						width: 640,
 						height: 480
 					}
@@ -35,16 +35,13 @@ class KakaoController < ApplicationController
 		elsif @response == "예약"
 			@msg = {
 				message: {
-					text: "예약 날짜 및 시간, 인원수를 입력해주세요.",
+					text: "예약 날짜 및 시간, 인원수를 입력해주세요."
 					# photo: {
 					#     url: "https://s3.ap-northeast-2.amazonaws.com/tongilstorage/kakaochat/3.PNG",
 					#     width: 640,
 					#     height: 480
 					# },
-					message_button: {
-						label: "UNI LION",
-						url: "https://uni.likelion.org/lectures"
-					}
+					
 				},
 				keyboard: {
 					type: "text"
@@ -52,5 +49,18 @@ class KakaoController < ApplicationController
 			}
 			render json: @msg, status: :ok
 		end
+		else
+			@msg = {
+				message: {
+					text: "#{@response}, 메시지가 잘 전달되었습니다."
+				},
+				keyboard: {
+					type: "buttons",
+					buttons: ["소개", "예약"]
+				}
+			}
+			render json: @msg, status: :ok
+		end
+			
 	end
 end
